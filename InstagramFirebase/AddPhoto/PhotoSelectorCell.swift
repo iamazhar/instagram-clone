bd07c986a13add429e00c2f8953801fa95110e11
Azhar <rahzadesign@gmail.com>
2018-10-11 23:46:00 +0530

    Setup photos framework to display users images in the cell grid

---
+++ InstagramFirebase/AddPhoto/PhotoSelectorCell.swift
@@ -0,0 +1,33 @@
+//
+//  PhotoSelectorCell.swift
+//  InstagramFirebase
+//
+//  Created by Azhar Anwar on 11/10/18.
+//  Copyright © 2018 Azhar Anwar. All rights reserved.
+//
+
+import UIKit
+
+class PhotoSelectorCell: UICollectionViewCell {
+    
+    let photoImageView: UIImageView = {
+        let iv = UIImageView()
+        iv.backgroundColor = UIColor.lightGray
+        iv.contentMode = .scaleAspectFill
+        iv.clipsToBounds = true
+        return iv
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+       
+        addSubview(photoImageView)
+        photoImageView.anchor(top: topAnchor, left: leftAnchor, bottom: bottomAnchor, right: rightAnchor, paddingTop: 0, paddingLeft: 0, paddingBottom: 0, paddingRight: 0, width: 0, height: 0)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+}
