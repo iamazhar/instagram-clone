01995790948272d059b3d46d34efd40c8808029d
Azhar <rahzadesign@gmail.com>
2018-10-18 13:09:51 +0530

    setup search service

--- InstagramFirebase/Search/UserSearchCell.swift
+++ InstagramFirebase/Search/UserSearchCell.swift
@@ -10,6 +10,15 @@ import UIKit
 
 class UserSearchCell: UICollectionViewCell {
     
+    var user: User? {
+        didSet {
+            usernameLabel.text = user?.username
+            
+            guard let profileImageUrl = user?.profileImageUrl else { return }
+            profileImageView.loadImage(urlString: profileImageUrl)
+        }
+    }
+    
     let profileImageView: CustomeImageView = {
         let iv = CustomeImageView()
         iv.backgroundColor = .red
